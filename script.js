7ac21b062767da52d0b9bec075d275ef7dbf3f7e
Gilson Rodrigues de Almeida <gilson.almeida@al.infnet.edu.br>
2021-11-19 06:19:58 -0300

    second commit

--- script.js
+++ script.js
@@ -7,18 +7,47 @@ class Perguntas {
         this.alternativas = alternativas;
     }
 }
+
+class Alternativa {
+    constructor(titulo, pontuacao) {
+        this.titulo = titulo;
+        this.pontuacao = pontuacao;
+    }
+}
+
 let perguntas = [
-    new Perguntas(titulo = "Escolha uma ferramenta de limpeza/cuidado doméstico para jogar Quadribol:",
-        alternativas = ["Ancinho", "Esfregão", "Vaporizador", "Roomba", "Aspirador de pó"]),
-    new Perguntas(titulo = "Escolha uma cor:", 
-        alternativas = ["Verde", "Azul", "Amarelo", "Vermelho"]),
-    new Perguntas(titulo = "Você é um bruxo?", 
-        alternativas = ["sim", "não"]),
-    new Perguntas(titulo = "Escolha um feitiço", 
-        alternativas = ["Expectro petroleum", "Hocus Pocus", "Havada cadebera"]),
-    new Perguntas(titulo = "Você está usando meias?", 
-        alternativas = ["sim", "não"])
+    new Perguntas(
+        titulo = "Escolha uma ferramenta de limpeza/cuidado doméstico para jogar Quadribol:",
+        alternativas = [
+            new Alternativa(titulo = "Ancinho", pontuacao = 3),
+            new Alternativa(titulo = "Esfregão", pontuacao = 0),
+            new Alternativa(titulo = "Roomba", pontuacao = 2),
+            new Alternativa(titulo = "Aspirador de pó", pontuacao = 1)]),
+    new Perguntas(
+        titulo = "Escolha uma cor:",
+        alternativas = [
+            new Alternativa(titulo = "Verde", pontuacao = 1),
+            new Alternativa(titulo = "Azul", pontuacao = 3),
+            new Alternativa(titulo = "Amarelo", pontuacao = 2),
+            new Alternativa(titulo = "Vermelho", pontuacao = 0)]),
+    new Perguntas(
+        titulo = "Você é um bruxo?",
+        alternativas = [
+            new Alternativa(titulo = "sim", pontuacao = 1),
+            new Alternativa(titulo = "não", pontuacao = 0)]),
+    new Perguntas(
+        titulo = "Escolha um feitiço",
+        alternativas = [
+            new Alternativa(titulo = "Expectro petroleum", pontuacao = 1),
+            new Alternativa(titulo = "Hocus Pocus", pontuacao = 3),
+            new Alternativa(titulo = "Havada cadebera", pontuacao = 2)]),
+    new Perguntas(
+        titulo = "Você está usando meias?",
+        alternativas = [
+            new Alternativa(titulo = "sim", pontuacao = 1),
+            new Alternativa(titulo = "não", pontuacao = 0)]),
 ]
+
 totalPontos = 0
 let perguntasHtml = document.getElementById("perguntas")
 
@@ -27,9 +56,9 @@ for (let i in perguntas) {
     let alternativas = ""
 
     for (index in pergunta.alternativas) {
-        alternativas += `<input type="radio" id="alternativa${index}" 
-        name="${i}" value="${index}">
-        <label for="alternativas${index}">${pergunta.alternativas[index]}</label><br/>`
+        alternativas += `<input type="radio" id="alternativa${index}"
+        name="${i}" value="${pergunta.alternativas[index].pontuacao}">
+        <label for="alternativas${index}">${pergunta.alternativas[index].titulo}</label><br/>`
     }
 
     perguntasHtml.innerHTML += `<div class="pergunta">
@@ -42,20 +71,22 @@ for (let i in perguntas) {
 function corrigir() {
     let pontos = 0
     let resultadoHtml = document.getElementById("resultado")
+    let botaoHtml = document.getElementById("verResultado")
+    botaoHtml.style.display = "none";
 
-    for(let elemento of document.querySelectorAll('input[type="radio"]:checked')) {
+    for (let elemento of document.querySelectorAll('input[type="radio"]:checked')) {
         pontos += parseInt(elemento.value)
     }
-    if( pontos <= 2) {
-        resultadoHtml.innerHTML = `Você é da casa Grifinória com ${pontos} pontos`
+    if (pontos <= 2) {
+        resultadoHtml.innerHTML = `<span class="grifinoria">Você é da casa Grifinória com ${pontos} pontos`
     }
-    if( pontos >= 3 && pontos <= 5) {
-        resultadoHtml.innerHTML = `Você é da casa Sonserina com ${pontos} pontos`
+    if (pontos >= 3 && pontos <= 5) {
+        resultadoHtml.innerHTML = `<span class="sonserina">Você é da casa Sonserina com ${pontos} pontos`
     }
-    if( pontos >= 6 && pontos <= 8) {
-        resultadoHtml.innerHTML = `Você é da casa Lufa Lufa com ${pontos} pontos`
+    if (pontos >= 6 && pontos <= 8) {
+        resultadoHtml.innerHTML = `<span class="lufalufa">Você é da casa Lufa Lufa com ${pontos} pontos`
     }
-    if( pontos >= 9) {
-        resultadoHtml.innerHTML = `Você é da casa Corvinal com ${pontos} pontos`
+    if (pontos >= 9) {
+        resultadoHtml.innerHTML = `<span class="corvinal">Você é da casa Corvinal com ${pontos} pontos</span>`
     }
 }
